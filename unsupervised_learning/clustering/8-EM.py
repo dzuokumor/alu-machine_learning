7953c193b6ae1785c1e47c42fe7d87b3735c4da5
dzuokumor <d.zuokumor@alustudent.com>
2026-03-18 11:49:11 +0100

    fix 8-EM and 9-BIC clustering files

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-'''
-Exoectation Maximization for a Gaussian Mixture Model
-'''
-
+"""Expectation Maximization for a Gaussian Mixture Model."""
 
 import numpy as np
 initialize = __import__('4-initialize').initialize
@@ -10,26 +7,37 @@ expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
 
-def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    '''
-    Expectation maximization for a GMM
-    '''
-    if not isinstance(X, np.ndarray):
+def expectation_maximization(X, k, iterations=1000, tol=1e-5,
+                             verbose=False):
+    """Perform the expectation maximization for a GMM."""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if not isinstance(k, int):
+    if not isinstance(k, int) or k <= 0:
         return None, None, None, None, None
-    if not isinstance(iterations, int):
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None, None
-    if not isinstance(tol, float):
+    if not isinstance(tol, (int, float)) or tol < 0:
         return None, None, None, None, None
     if not isinstance(verbose, bool):
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
-    g, l = expectation(X, pi, m, S)
+    g, li = expectation(X, pi, m, S)
+
+    if verbose:
+        print("Log Likelihood after {} iterations: {}"
+              .format(0, round(li, 5)))
+
     for i in range(iterations):
+        prev_li = li
         pi, m, S = maximization(X, g)
-        g, l = expectation(X, pi, m, S)
-        if verbose and i % 10 == 0:
-            print(f"Log Likelihood after {i} iterations: {l:.5f}")
-    return pi, m, S, g, l
+        g, li = expectation(X, pi, m, S)
+
+        if verbose and ((i + 1) % 10 == 0 or abs(li - prev_li) <= tol):
+            print("Log Likelihood after {} iterations: {}"
+                  .format(i + 1, round(li, 5)))
+
+        if abs(li - prev_li) <= tol:
+            break
+
+    return pi, m, S, g, li
--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
-"""
-Defines function that finds the best number of clusters for a GMM using
-the Bayesian Information Criterion (BIC)
-"""
-
+"""Find the best number of clusters for a GMM using the Bayesian
+Information Criterion."""
 
 import numpy as np
-expectation_maximization = __import__('7-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Find the best number of clusters for a GMM using BIC
-    """
-    return None, None, None, None
+    """Find the best number of clusters for a GMM using BIC."""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None
+    n, d = X.shape
+    if kmax is None:
+        kmax = n
+    if not isinstance(kmin, int) or kmin <= 0 or kmin > n:
+        return None, None, None, None
+    if not isinstance(kmax, int) or kmax <= 0 or kmax > n:
+        return None, None, None, None
+    if kmax <= kmin:
+        return None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, (int, float)) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
 
+    liks = np.empty(kmax - kmin + 1)
+    bics = np.empty(kmax - kmin + 1)
+    results = []
+
+    for idx, k in enumerate(range(kmin, kmax + 1)):
+        pi, m, S, g, li = expectation_maximization(
+            X, k, iterations, tol, verbose)
+        results.append((pi, m, S))
+        liks[idx] = li
+
+        # number of parameters: k*d (means) + k*d*(d+1)/2 (covariances)
+        # + (k-1) (priors)
+        p = k * d + k * d * (d + 1) / 2 + (k - 1)
+        bics[idx] = p * np.log(n) - 2 * li
+
+    best_idx = np.argmin(bics)
+    best_k = best_idx + kmin
+    best_result = results[best_idx]
+
+    return best_k, best_result, liks, bics
