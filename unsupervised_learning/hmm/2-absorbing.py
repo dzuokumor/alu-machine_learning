359ff06f37dfc5fb534e100f1f79c743d5dcf5c0
dzuokumor <d.zuokumor@alustudent.com>
2026-03-18 12:02:06 +0100

    fix absorbing chain detection

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
-'''
-    function def absorbing(P): that
-    determines if a markov chain is absorbing
-'''
+"""Determines if a markov chain is absorbing."""
 
 
 import numpy as np
 
 
 def absorbing(P):
-    '''
-        Determines if a markov chain is absorbing
-    '''
-    if len(P.shape) != 2:
-        return None
+    """Determine if a markov chain is absorbing."""
+    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
+        return False
     n1, n2 = P.shape
-    if (n1 != n2) or type(P) is not np.ndarray:
-        return None
-    D = np.diagonal(P)
-    if (D == 1).all():
-        return True
-    if not (D == 1).any():
+    if n1 != n2:
         return False
 
-    for i in range(n1):
-            # print('this is Pi {}'.format(P[i]))
-            for j in range(n2):
-                # print('this is Pj {}'.format(P[j]))
-                if (i == j) and (i + 1 < len(P)):
-                    if P[i + 1][j] == 0 and P[i][j + 1] == 0:
-                        return False
-    return True
+    D = np.diagonal(P)
+    if not (D == 1).any():
+        return False
+    if (D == 1).all():
+        return True
+
+    absorb = set(np.where(D == 1)[0])
+    reachable = set(np.where(D == 1)[0])
+
+    changed = True
+    while changed:
+        changed = False
+        for i in range(n1):
+            if i not in reachable:
+                for j in reachable:
+                    if P[i][j] > 0:
+                        reachable.add(i)
+                        changed = True
+                        break
+
+    return len(reachable) == n1
